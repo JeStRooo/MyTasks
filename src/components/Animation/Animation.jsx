e82a683dbe7201886ada79f5f2cc17a18d7af9e3
JeStRooo <euroset270578@gmail.com>
2023-03-18 14:50:56 +0300

    Update Animation

--- src/components/Animation/Animation.jsx
+++ src/components/Animation/Animation.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import classes from "./Animation.module.css";
+import classes from "./Animation.module.scss";
 import page from "../../assets/page.gif";
 
 const Animation = () => {
+  const [isModal, setIsModal] = useState(true)
+
+  const showModal = () => {
+    setIsModal(!isModal)
+  }
+
   return (
-    <div className={classes.modal}>
-      <h1 className={classes.modal__title}>Это что, модальное окно?</h1>
-      <img src={page} alt="Ой!" className={classes.modal__img}/>
-    </div>
-  );
+    <>
+      <button className={classes.button} onClick={showModal}>
+        {!isModal ? 'Показать модальное окно' : 'Скрыть модальное окно'}
+      </button>
+      <div className={isModal ? classes.modal : classes.not_modal}>
+        <h1 className={classes.modal__title}>Это что, модальное окно?</h1>
+        <img src={page} alt="Ой!" className={classes.modal__img}/>
+      </div>
+    </>
+  )
+    ;
 };
 
 export default Animation;
